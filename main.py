1fb72b7d7624f1eeaedf9af783fe42e4cbdc19dc
Sungsoo <onair0817@gmail.com>
2022-12-22 17:31:44 +0900

    feat: add path parameter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -9,9 +11,41 @@ class Item(BaseModel):
     tax: float = None
 
 
+class BankType(str, Enum):
+    kakaobank = "카카오뱅크"
+    kbank = "케이뱅크"
+    toss = "토스"
+
+
 app = FastAPI()
 
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users/me")
+async def read_me():
+    return {"user": "the current user"}
+
+
+@app.get("/users/{name}")
+async def read_user_name(name: str):
+    return {"user": name}
+
+
 @app.post("/items/")
-async def create_item(item: Item):
-    return item
+async def read_item(item: Item):
+    return {"item": item}
+
+
+@app.get("/bank-types/{bank_type}")
+async def get_intro(bank_type: BankType):
+    print(bank_type)
+    if bank_type == BankType.kakaobank:
+        return {"bank_type": bank_type, "msg": "같지만 다른 은행"}
+    elif bank_type == BankType.kbank:
+        return {"bank_type": bank_type, "msg": "모바일로 만나는 1금융권 은행"}
+    elif bank_type == BankType.toss:
+        return {"bank_type": bank_type, "msg": "새로운 은행을 만날 시간"}

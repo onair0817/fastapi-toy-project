c258c83643551e4280c29c19dad4fc30b67d1413
Sungsoo <onair0817@gmail.com>
2022-12-22 14:46:58 +0900

    chore: update python packages

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
 
 app = FastAPI()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item

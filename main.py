226d4276be3ac7eda5ece49b399dd2f278e86fe6
Sungsoo <onair0817@gmail.com>
2022-12-23 15:09:36 +0900

    chore: add flake8 config file - ignore E203 - default options

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Item(BaseModel):
@@ -42,10 +43,24 @@ async def read_item(item: Item):
 
 @app.get("/bank-types/{bank_type}")
 async def get_intro(bank_type: BankType):
-    print(bank_type)
     if bank_type == BankType.kakaobank:
         return {"bank_type": bank_type, "msg": "같지만 다른 은행"}
     elif bank_type == BankType.kbank:
         return {"bank_type": bank_type, "msg": "모바일로 만나는 1금융권 은행"}
     elif bank_type == BankType.toss:
         return {"bank_type": bank_type, "msg": "새로운 은행을 만날 시간"}
+
+
+fake_items_db = [{"name": "Ted"}, {"name": "Robin"}, {"name": "Barney"}]
+
+
+@app.get("/info/")
+async def read_info(skip: int = 0, limit: int = 2):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/tv-shows/{tv_show_name}")
+async def read_tv_show(tv_show_name: str, character: Optional[str] = None):
+    if character:
+        return {"tv_show_name": tv_show_name, "character": character}
+    return {"tv_show_name": tv_show_name}
--- req.py
+++ req.py
@@ -3,7 +3,7 @@ import json
 
 
 # ---
-# api /items/
+# 1. api /items/
 # ---
 
 # request url
@@ -19,13 +19,40 @@ res = requests.post(url, data=json.dumps(data))
 print(res.text)
 
 # ---
-# api /bank_types/
+# 2. api /bank_types/
 # ---
 
 bank_list = ["카카오뱅크", "케이뱅크", "토스"]
 
+# get request to api
 for bank in bank_list:
     url = f"http://localhost:8000/bank-types/{bank}"
-    print(url)
     res = requests.get(url)
     print(res.text)
+
+# ---
+# 3. api /info/
+# ---
+
+# request url
+url = "http://localhost:8000/info/"
+
+# get request to api
+res = requests.get(url)
+
+# print response
+print(res.text)
+
+# ---
+# 4. api /tv_show_name/
+# ---
+
+# request url
+tv_show = "noltoe"
+url = f"http://localhost:8000/tv-shows/{tv_show}"
+
+# get request to api
+res = requests.get(url)
+
+# print response
+print(res.text)

8f40d43cf859b8635ada7d8652ab10481eb999b5
Sungsoo <onair0817@gmail.com>
2022-12-23 17:36:45 +0900

    feat: update api example cases

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 
 class Item(BaseModel):
@@ -60,7 +60,36 @@ async def read_info(skip: int = 0, limit: int = 2):
 
 
 @app.get("/tv-shows/{tv_show_name}")
+# or Optional[str] = Query(None)으로 설정하면 쿼리 매개변수 값 검증 수행 e.g. 글자수 10자 이내인지
+# 만일 조건을 구체화하고 싶으면 Query(None, min_length=3, max_length=10, regex="^show$")로 추가 가능
+# 필수값으로 받으려면 None 대신 ...으로 대체
 async def read_tv_show(tv_show_name: str, character: Optional[str] = None):
     if character:
         return {"tv_show_name": tv_show_name, "character": character}
     return {"tv_show_name": tv_show_name}
+
+
+@app.get("/tv-shows/{tv_show_name}/characters")
+async def read_tv_show_and_character(tv_show_name: str, character: str):
+    return {"tv_show_name": tv_show_name, "character": character}
+
+
+@app.get("/movies/{movie}")
+async def read_user_item(movie: str, character: str):
+    item = {"movie": movie, "needy": character}
+    return item
+
+
+@app.get("/characters/")
+# Query(None) 대신 Query(['white', 'motive', 'doy']) 넣으면 빈 요청시 기본값으로 반환함
+async def read_characters(q: Optional[List[str]] = Query(["white", "motive", "doy"])):
+    query_items = {"q": q}
+    return query_items
+
+
+@app.get("/numbers/{number_value}")
+# ge: greater than or equal, lt: less than
+async def read_number(
+    number_value: int = Path(..., title="The value of number", ge=100)
+):
+    return {"number": number_value}
--- req.py
+++ req.py
@@ -8,13 +8,10 @@ import json
 
 # request url
 url = "http://localhost:8000/items/"
-
 # data
 data = {"name": "new_challenge", "description": "test1", "price": 2020, "tax": 2021}
-
 # post request to api
 res = requests.post(url, data=json.dumps(data))
-
 # print response
 print(res.text)
 
@@ -23,7 +20,6 @@ print(res.text)
 # ---
 
 bank_list = ["카카오뱅크", "케이뱅크", "토스"]
-
 # get request to api
 for bank in bank_list:
     url = f"http://localhost:8000/bank-types/{bank}"
@@ -36,10 +32,8 @@ for bank in bank_list:
 
 # request url
 url = "http://localhost:8000/info/"
-
 # get request to api
 res = requests.get(url)
-
 # print response
 print(res.text)
 
@@ -50,9 +44,60 @@ print(res.text)
 # request url
 tv_show = "noltoe"
 url = f"http://localhost:8000/tv-shows/{tv_show}"
-
 # get request to api
 res = requests.get(url)
+# print response
+print(res.text)
 
+# ---
+
+url = "http://127.0.0.1:8000/tv-shows/HIMYM/characters?character=Ted"
+# get request to api
+res = requests.get(url)
+# print response
+print(res.text)
+
+# ---
+# 5. api /movies/
+# ---
+
+# error case
+# url = "http://127.0.0.1:8000/movies/comet"
+# normal case
+url = "http://127.0.0.1:8000/movies/comet?character=Kimberley"
+# get request to api
+res = requests.get(url)
+# print response
+print(res.text)
+
+# ---
+# 6. api /characters/
+# ---
+
+url = "http://127.0.0.1:8000/characters/?q=ted&q=robin&q=barney"
+# get request to api
+res = requests.get(url)
+# print response
+print(res.text)
+
+url = "http://127.0.0.1:8000/characters/"
+# get request to api
+res = requests.get(url)
+# print response
+print(res.text)
+
+# ---
+# 7. api /numbers/
+# ---
+
+url = "http://127.0.0.1:8000/numbers/50"
+# get request to api
+res = requests.get(url)
+# print response
+print(res.text)
+
+url = "http://127.0.0.1:8000/numbers/200"
+# get request to api
+res = requests.get(url)
 # print response
 print(res.text)
